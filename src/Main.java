d228bb5a72430fd46831577faee1ec1bfa6aa7d8
NISCHAL GAUTAM <nischalgautam7777@gmail.com>
2023-01-04 18:29:58 +0530

    java practise code file

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        int[] array1 = {2,3,3,5,6,5};
+        array1[3]=8;
+        System.out.println(array1);
+        System.out.println("Write a name here: \n");
+         Scanner sc = new Scanner(System.in);
+         String name = sc.next();
+         switch (name){
+             case "nischal":
+                 System.out.println("thats your official name");
+                 break;
+
+             case "nisu":
+                 System.out.println("Thats what  people at home call you");
+                 break;
+
+
+             case "lavaris":
+                 System.out.println("Thats what 8 floor c9 people call you");
+                 break;
+
+             default:
+                 System.out.println("nt a valid name");
+         }*/
+
+
+
+
+    }
+}
