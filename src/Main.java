890b5d39190b995e316bd74fb9d52d13cc27b935
NISCHAL GAUTAM <nischalgautam7777@gmail.com>
2023-01-25 10:57:33 +0530

    java practise code file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -33,6 +31,115 @@ public class Main {
              default:
                  System.out.println("nt a valid name");
          }*/
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+        //int userInput = sc.nextInt();
+
+        //for(int i=0;i<5;i++){
+
+            //System.out.println("yo bro number"+userInput+i);
+       //}
+        /*System.out.println("heres the table of"+userInput);
+
+        int x = 0;
+        while(x<11){
+            System.out.println(x * userInput);
+            x++;
+        }
+
+
+
+
+        int[] a = new int[5];
+        for(int i=0;i<5;i++){
+            a[i] =sc.nextInt();
+            System.out.println("");
+        }
+
+
+        String[] b = new String[5];
+        for (int i=0;i<5;i++){
+            b[i]= sc.next();
+        }
+
+        //int[] c = Arrays.copyOf(a,4);
+
+        Arrays.sort(b);
+        Arrays.sort(a);
+
+
+        //boolean ifEqual = Arrays.equals(a,b);
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(a));
+
+
+        ArrayList<Integer> arrli = new ArrayList<Integer>(5);
+        for(int i=0;i<9;i++){
+            arrli.add(sc.nextInt());
+
+        }
+
+
+        for (int i = 0;i<5;i++)
+            System.out.println(arrli.get(i) + " ");
+
+
+
+        int num = sc.nextInt();
+
+        if (num <10){
+            System.out.println("Good morning");
+        }
+
+
+        else if (num<18) {
+            System.out.println("Good day");
+
+        }
+
+
+
+
+        else{
+            System.out.println("Good evening");
+        }
+
+
+
+        Integer a =10;
+        Double  b = 30.54;
+        Character c = 'g';
+
+
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println("b");
+        System.out.println(b);
+        System.out.println("c");
+        System.out.println(c);
+
+        */
+         Integer a = 70;
+         Integer b = 70;
+         int ans = Integer.compare(a , b);
+        System.out.println(ans);
+        int ans2 = Integer.sum(a,b);
+        System.out.println(ans2);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
